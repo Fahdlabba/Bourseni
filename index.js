63521abd7de80094d6d03570e7cb4337851fa0c7
fahd <labbafahd012@gmail.com>
2024-05-01 15:18:37 +0100

    final  commit

--- index.js
+++ index.js
@@ -21,4 +21,29 @@ const progressCircle = document.querySelector(".autoplay-progress svg");
           progressContent.textContent = `${Math.ceil(time / 1000)}s`;
         }
       }
-    });
+});
+
+
+
+
+
+
+
+let landingImage = document.querySelector(".landing");
+landingImage.style.backgroundImage = `url(./src/cours-a-distance-scaled.jpg)`;
+landingImage.style.backgroundSize = "cover";
+
+
+
+
+let logo = document.createElement("img");
+
+logo.src = "./src/symbole-bourse-etudes-logo-vectoriel-piece-dollar-portant-chapeau-graduation-pret-etudiant-pret-argent-pour-education_642540-247.avif";
+
+let imageConatiner = document.querySelector(".left-footer");
+
+imageConatiner.appendChild(logo);
+
+
+let logoContainer = document.querySelector(".logo")
+logoContainer.appendChild(logo);
